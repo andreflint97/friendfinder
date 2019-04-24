546eb6a9b043a2619d0b0e5ea170cf0458a27736
andre flint <andreflint97@icloud.com>
2019-04-24 18:20:03 -0400

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+	{
+		name: "Noah",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Eric",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Kenny",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Brandon",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Frank",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
